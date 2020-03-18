634f8b3431f0673004694d61807ea28e16b06ed8
Richie Bendall <richiebendall@gmail.com>
2020-03-19 00:25:16 +1300

    chore: Update dependencies
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const mergeOptions = require("merge-options").bind({ ignoreUndefined: true })
 const setup = require("hsimp-purescript")
 
 const defaultConfig = {
-    calculation: {
-        calcs: 40e9,
-        characterSets: require("hsimp-purescript/dictionaries/character-sets"),
-    },
-    time: {
-        periods: require("hsimp-purescript/dictionaries/periods"),
-        namedNumbers: require("hsimp-purescript/dictionaries/named-numbers"),
-        forever: "Forever",
-        instantly: "Instantly",
-    },
-    checks: {
-        dictionary: require("hsimp-purescript/dictionaries/top10k"),
-        patterns: require("hsimp-purescript/dictionaries/patterns"),
-        messages: require("hsimp-purescript/dictionaries/checks"),
-    },
+	calculation: {
+		calcs: 40e9,
+		characterSets: require("hsimp-purescript/dictionaries/character-sets"),
+	},
+	time: {
+		periods: require("hsimp-purescript/dictionaries/periods"),
+		namedNumbers: require("hsimp-purescript/dictionaries/named-numbers"),
+		forever: "Forever",
+		instantly: "Instantly",
+	},
+	checks: {
+		dictionary: require("hsimp-purescript/dictionaries/top10k"),
+		patterns: require("hsimp-purescript/dictionaries/patterns"),
+		messages: require("hsimp-purescript/dictionaries/checks"),
+	},
 }
 
 module.exports = (password, config = {}) => {
-    assert.string(password)
-    assert.plainObject(config)
+	assert.string(password)
+	assert.plainObject(config)
 
-    return setup(mergeOptions(defaultConfig, config))(password)
+	return setup(mergeOptions(defaultConfig, config))(password)
 }
--- test.js
+++ test.js
@@ -1,26 +1,26 @@
-import test from "ava"
-import hsimp from "."
+const test = require("ava")
+const hsimp = require(".")
 
 test("main", (t) => {
-    t.deepEqual(hsimp("123"), {
-        time: "24 nanoseconds",
-        level: "warning",
-        checks: [
-            {
-                name: "Length: Very short",
-                message: "Your password is very short. The longer a password is the more secure it will be.",
-                level: "warning",
-            },
-            {
-                name: "Character Variety: Just Numbers",
-                message: "Your password only contains numbers. This reduces the number of possible combinations dramatically.",
-                level: "warning",
-            },
-            {
-                name: "Possibly a Telephone Number / Date",
-                message: "Your password looks like it might be a telephone number or a date. If it is and it has personal significance then it might be very easy for someone to guess.",
-                level: "warning",
-            },
-        ],
-    })
+	t.deepEqual(hsimp("123"), {
+		time: "24 nanoseconds",
+		level: "warning",
+		checks: [
+			{
+				name: "Length: Very short",
+				message: "Your password is very short. The longer a password is the more secure it will be.",
+				level: "warning",
+			},
+			{
+				name: "Character Variety: Just Numbers",
+				message: "Your password only contains numbers. This reduces the number of possible combinations dramatically.",
+				level: "warning",
+			},
+			{
+				name: "Possibly a Telephone Number / Date",
+				message: "Your password looks like it might be a telephone number or a date. If it is and it has personal significance then it might be very easy for someone to guess.",
+				level: "warning",
+			},
+		],
+	})
 })
